7186527d04d57b2e42f072bfd85385a3995b06e6
Lily Carey <lily.a.carey@gmail.com>
2019-07-21 04:17:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+}
+
+var array = [];
+for (var i = 0; i < musicians.length; i++){
+  array.push("${musicians[i]} plays ${instruments[i]}");
+  return array;
+}
+
+function johnLennonFacts(facts){
+  
+}
+
+var fact = 0;
+while (counter<facts.length){
+  facts[counter] = facts(0)
+}
